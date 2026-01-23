89ca7524413ebccb392e4986aad65062d60d509c
Pratyush Ranjan <144735922+Pratyush271299@users.noreply.github.com>
2026-01-23 11:29:19 +0530

    Update User.cs

--- E-Commerce/Data/User.cs
+++ E-Commerce/Data/User.cs
@@ -7,8 +7,8 @@ namespace E_Commerce.Data
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public byte[] Password { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        public string Password { get; set; }
+        public string PasswordSalt { get; set; }
         public string? Role { get; set; }
         public virtual ICollection<CartProduct> CartProducts { get; set; }
     }
